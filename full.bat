10fa487813b4ed7b4df44c73f78c1ce27e8e4e0e
zlc <929358166@qq.com>
2026-03-01 21:21:37 +0800

    add full.bat

---
+++ full.bat
@@ -0,0 +1,15 @@
+@echo off
+
+Rem Path to the executable exe of the Vector HexView tool on your PC
+set "HexViewPath=D:\sandbox\01_tools\HexView\HexView\hexview.exe"
+
+Rem Filled input file Input_Hex_File
+set "Input_Hex_File=D:\03_project\stm32\f103\01_code\FBL\stm32_Bootload_UDS\build\Debug\a.hex"  
+
+Rem The output file Output_Hex_File after filling
+set "Output_Hex_File=D:\03_project\stm32\f103\01_code\FBL\stm32_Bootload_UDS\build\Debug\a.hex"
+
+echo Filling the hex file to 32K...
+
+Rem For a new Output_Hex_File, fill in the characters
+%HexViewPath% %Input_Hex_File% /S /FA: /AF:0xFF /XI:32 -o %Output_Hex_File%
